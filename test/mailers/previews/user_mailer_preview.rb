4f5fee1d4098227cae7804717398155a71f102c8
m9795 <106650955+m9795@users.noreply.github.com>
2022-07-03 00:13:53 +0900

    [Add]Mailer作成

---
+++ test/mailers/previews/user_mailer_preview.rb
@@ -0,0 +1,9 @@
+# Preview all emails at http://localhost:3000/rails/mailers/user_mailer
+class UserMailerPreview < ActionMailer::Preview
+
+  # Preview this email at http://localhost:3000/rails/mailers/user_mailer/event_email
+  def event_email
+    UserMailer.event_email
+  end
+
+end
