85bc881ac8add4a61fd5a0f3db7f6cfbcba1c598
m9795 <106650955+m9795@users.noreply.github.com>
2022-07-02 23:33:51 +0900

    [Add]メール送信設定

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -75,4 +75,16 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "c337219d2b8d4cc4a0837b58bf2202bd.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'smtp.gmail.com',
+    user_name:            '<YOUR EMAIL ADDRESS>',
+    password:             '<YOUR EMAIL PASSWORD>',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
 end
